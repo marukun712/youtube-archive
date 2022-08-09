0881cf297895d4d8f46c74c33f5b72592ebbec0a
marukun_ <87289638+marukun712@users.noreply.github.com>
2022-08-09 09:45:54 +0900

    commit

--- js/index.js
+++ js/index.js
@@ -26,7 +26,6 @@ function start() {
         var channel = data.author_url
         let status = object[i].status
         let date = object[i].day
-
         card.insertAdjacentHTML("beforeend",
           `<div class="flex justify-center p-10">
         <div class="rounded-lg shadow-lg bg-white max-w-sm">
@@ -44,7 +43,7 @@ function start() {
             </p>
             <p class="text-gray-500 text-xl  mb-2">
             ${status}
-          </p>
+          </p> 
             <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" id=${i} onclick='getId(this)' value='close'>Watch</button>
             <button type="button" class=" inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" id=${i} onclick='remove(this)'>Delete</button></div>
         </div>
